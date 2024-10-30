b4522fce4be3b5a63d9cafaf21895c5f37eb1138
r-ofarrell <ryanof@pm.me>
2024-10-30 14:42:32 -0400

    Added spaceship movement

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print("Starting asteroids!")
